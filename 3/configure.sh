9ac4a1a43c341cd16015a37f58f20eb577ba2563
Yakup Ates <Yakup.Ates@rub.de>
2017-07-20 13:32:43 +0200

    solution

--- 3/configure.sh
+++ 3/configure.sh
@@ -2,7 +2,7 @@
 # Yakup Ates <Yakup.Ates@rub.de>
 # 20.07.2017
 
-#php vendor/autoload.php update twbs/bootstrap
+composer update twbs/bootstrap
 cp assets/css/style.css .
 rm -r assets
 cp -R vendor/twbs/bootstrap/dist/ assets/
